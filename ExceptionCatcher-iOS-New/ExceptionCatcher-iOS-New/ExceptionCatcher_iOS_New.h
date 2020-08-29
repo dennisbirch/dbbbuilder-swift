3218971729371f48535813f6f39312fd5a960b86
Dennis Birch <dbirchdev@gmail.com>
2020-08-28 17:44:50 -0700

    Created new iOS ExceptionCatcher project

---
+++ ExceptionCatcher-iOS-New/ExceptionCatcher-iOS-New/ExceptionCatcher_iOS_New.h
@@ -0,0 +1,19 @@
+//
+//  ExceptionCatcher_iOS_New.h
+//  ExceptionCatcher-iOS-New
+//
+//  Created by Dennis Birch on 8/28/20.
+//  Copyright © 2020 Dennis Birch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExceptionCatcher_iOS_New.
+FOUNDATION_EXPORT double ExceptionCatcher_iOS_NewVersionNumber;
+
+//! Project version string for ExceptionCatcher_iOS_New.
+FOUNDATION_EXPORT const unsigned char ExceptionCatcher_iOS_NewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExceptionCatcher_iOS_New/PublicHeader.h>
+
+
